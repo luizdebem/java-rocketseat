5aec916ecf7d78de96f4d8f994c7d75aeec918b7
luizdebem <luizdebem99@gmail.com>
2023-10-10 20:22:14 -0300

    aula 2: persistência com JPA e H2; modelagem do banco de dados e algumas constraints; lib Lombok; IRepository.

--- src/main/java/br/com/luizdebem/todolist/user/UserController.java
+++ src/main/java/br/com/luizdebem/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.luizdebem.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+  // autowired gerencia o ciclo de vida da instância nesse caso do userRepository
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.fullName);
+  public ResponseEntity<?> create(@RequestBody UserModel userModel) {
+    UserModel user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists.");
+    }
+
+    UserModel createdUser = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.OK).body(createdUser);
   }
 }
--- src/main/java/br/com/luizdebem/todolist/user/UserModel.java
+++ src/main/java/br/com/luizdebem/todolist/user/UserModel.java
@@ -1,7 +1,31 @@
 package br.com.luizdebem.todolist.user;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+// Lombok: lib que facilita os getters e setters, não precisa nem especificar.
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Entity(name="tb_users")
 public class UserModel {
-  public String username;
-  public String fullName;
-  public String password;
+  @Getter
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  @Column(unique = true)
+  private String username;
+  private String fullname;
+  private String password;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
 }
